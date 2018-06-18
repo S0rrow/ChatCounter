69e416a396c94d15f57bdc2e700265be92a46854
S0rrow <jae9hell@gmail.com>
2018-06-18 17:37:52 +0900

    180618PM0537

--- build/scripts/HW3.bat
+++ build/scripts/HW3.bat
@@ -63,10 +63,10 @@ set CMD_LINE_ARGS=%*
 :execute
 @rem Setup the command line
 
-set CLASSPATH=%APP_HOME%\lib\HW3.jar
+set CLASSPATH=%APP_HOME%\lib\HW3.jar;%APP_HOME%\lib\commons-math3-3.6.1.jar;%APP_HOME%\lib\commons-cli-1.4.jar;%APP_HOME%\lib\guava-23.0.jar;%APP_HOME%\lib\jsr305-1.3.9.jar;%APP_HOME%\lib\error_prone_annotations-2.0.18.jar;%APP_HOME%\lib\j2objc-annotations-1.1.jar;%APP_HOME%\lib\animal-sniffer-annotations-1.14.jar
 
 @rem Execute HW3
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %H_W3_OPTS%  -classpath "%CLASSPATH%" edu.handong.csee.java.hw3.MainHW3.java %CMD_LINE_ARGS%
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %H_W3_OPTS%  -classpath "%CLASSPATH%" edu.handong.csee.java.hw3.MainHW3 %CMD_LINE_ARGS%
 
 :end
 @rem End local scope for the variables with windows NT shell
